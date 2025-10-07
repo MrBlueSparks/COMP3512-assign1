6d20bcf7f659013e5506a3d250f5eebab7df6975
MrBlueSparks <74160105+MrBlueSparks@users.noreply.github.com>
2025-10-06 19:06:47 -0600

    fully setup file structure

---
+++ rbark369/index.php
@@ -0,0 +1,22 @@
+<php 
+//home page
+
+
+
+//html 
+
+?>
+<html>
+<head>
+    <title>Home Page</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <h1>Welcome to the Home Page</h1>
+    <p>This is a simple home page.</p>
+</body>
+
+</html>
+<?php
+
+?>
