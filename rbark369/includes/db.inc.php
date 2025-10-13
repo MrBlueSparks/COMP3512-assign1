1c1b40bdaf9edf7451cc8ee7fbc877fa10322b5a
MrBlueSparks <74160105+MrBlueSparks@users.noreply.github.com>
2025-10-13 12:27:43 -0600

    started helper file

---
+++ rbark369/includes/db.inc.php
@@ -0,0 +1,10 @@
+<?php
+//helper file for database connection
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBNAME', 'rbark369');
+define('DBCONNSTRING', 'mysql:host=' . DBHOST . ';dbname=' . DBNAME);
+
+
+?>
