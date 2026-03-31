eaaf818066f2c02a3f78cdf6e8b6f35246d1942b
MrBlueSparks <74160105+MrBlueSparks@users.noreply.github.com>
2026-03-31 17:38:02 -0600

    Fix: normalize symbol parameter to uppercase in APIs

--- rbark369/api/companies.php
+++ rbark369/api/companies.php
@@ -9,7 +9,7 @@ include '../includes/db.inc.php';
 //checks if there is ref parameter
 
 if (isset($_GET['ref'])){
-    $symbol = $_GET['ref'];
+    $symbol = strtoupper($_GET['ref']);
     $sql = "SELECT * FROM companies WHERE symbol = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bindValue(1, $symbol);
